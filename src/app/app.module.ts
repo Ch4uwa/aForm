14e3cfd47b71f0f99b1a91326981760ce2f7a1ac
Martin Karlsson <mrtn.karlsson@gmail.com>
2021-10-30 17:32:45 +0200

    added TODO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+/**
+ * TODO: Change the use of "of".
+ */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
